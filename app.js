afa0c9f69c66303c65a09cab33e732c338ea5017
RaymondDashWu <raymond31670@hotmail.com>
2018-08-31 20:47:58 -0700

    initial commit rotten potatoes tutorial

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('home', {msg: 'Hello World!'});
+    //res.render('home', {msg: 'Hello World!'});
+    res.render('reviews-index', {reviews: reviews});
+
 })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
+
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
